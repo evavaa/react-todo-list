fae33211a67584afe18c3e8c981896ae8c4d4294
evavaa <evach0305@gmail.com>
2025-02-27 23:53:09 +1100

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/react-todo-list/'
-  }
-
-  return config
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/react-todo-list/',
 })
