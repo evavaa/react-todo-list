00f7d11b1f68f449ed7516412b0a51700a4871ab
evavaa <evach0305@gmail.com>
2025-02-27 23:43:49 +1100

    update vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-todo-list/'
+  }
+
+  return config
 })
